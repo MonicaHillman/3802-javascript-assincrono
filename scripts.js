7025c37fe3d1a8b0a489f4223db971983bd1c463
monicahillman <mmhillman1@ucs.br>
2024-06-20 08:24:32 -0300

    projeto base

--- scripts.js
+++
@@ -1,6 +0,0 @@
-const uploadBtn = document.getElementById("upload-btn");
-const inputUpload = document.getElementById("image-upload")
-
-uploadBtn.addEventListener("click", () => {
-    inputUpload.click();
-})
