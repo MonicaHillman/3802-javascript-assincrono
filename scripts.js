6e8a6e1aa3e791e9dfb9dd6cebf658f418c1c26b
monicahillman <mmhillman1@ucs.br>
2024-06-20 08:23:40 -0300

    aula 01

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const uploadBtn = document.getElementById("upload-btn");
+const inputUpload = document.getElementById("image-upload")
+
+uploadBtn.addEventListener("click", () => {
+    inputUpload.click();
+})
